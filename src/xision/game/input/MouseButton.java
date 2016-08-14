89481bb686862a50541378192ce07d61b6bdb230
Connor Moody <cwmlolzlz@gmail.com>
2016-08-14 22:12:58 +1200

    Converted to enum

--- src/xision/game/input/MouseButton.java
+++ src/xision/game/input/MouseButton.java
@@ -3,8 +3,12 @@ package xision.game.input;
 /**
  * Created by Connor on 7/08/2016.
  */
-public class MouseButton{
-    public static final int LEFT = 1;
-    public static final int RIGHT = 2;
-    public static final int MIDDLE = 3;
+public enum MouseButton{
+    LEFT,
+    RIGHT,
+    MIDDLE;
+
+    public static MouseButton fromInt(int i){
+        return MouseButton.values()[i];
+    }
 }
