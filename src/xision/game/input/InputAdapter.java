a54812bde94686c5b24c88a3788eb77c90763332
Connor Moody <cwmlolzlz@gmail.com>
2016-08-14 22:13:55 +1200

    Added InputAdapter to collect all interface methods from MouseInputListener and KeyListener

---
+++ src/xision/game/input/InputAdapter.java
@@ -0,0 +1,61 @@
+package xision.game.input;
+
+import javax.swing.event.MouseInputListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+
+/**
+ * Created by Connor on 14/08/2016.
+ */
+public abstract class InputAdapter implements MouseInputListener, KeyListener{
+    @Override
+    public void keyTyped(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e){
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e){
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e){
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e){
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e){
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e){
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e){
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e){
+
+    }
+}
