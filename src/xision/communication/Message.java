9d71f69f791aa757cac892aa4bf8b81529bd1c04
Connor Moody <cwmlolzlz@gmail.com>
2016-08-16 22:02:52 +1200

    Added Message class documentation

--- src/xision/communication/Message.java
+++ src/xision/communication/Message.java
@@ -4,8 +4,15 @@ import java.io.Serializable;
 
 /**
  * Created by Connor on 8/08/2016.
+ *
+ * The low level interface to encapsulate data sent between
+ * connections. This can be used internally or sent across network
+ *
+ * Note: When extending this class be careful when adding Objects
+ * as fields. This class must be serializable and therefore any
+ * contained fields must also be marked Serializable.
  */
-public abstract class Message implements Serializable{
+public interface Message extends Serializable{
 
     /*public enum TransmissionMode{
         ECHO,
